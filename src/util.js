df6e776e2d2efba48d6eecb1f7e32ab56c85fd7b
Kai Klostermann <kai@odd.af>
2021-12-25 15:58:33 +0100

    Improved the readability of sanitizeTabIndexInput Improved documentation of sanitizieTabIndexInput

--- src/util.js
+++ src/util.js
@@ -14,15 +14,25 @@ export function sortByTabIndex(firstNode, secondNode) {
     .reduce((previousValue, currentValue) => previousValue - currentValue);
 }
 
-function sanitizeTabIndexInput(tabIndex, maxValue) {
+/**
+ * Prepares a tab-index to be further processed for the tab order of the focus trap.
+ * It can't be less than 0, because negative values can not be part of the tab order at all.
+ * In case it's exactly 0 it actually needs to be higher than any positive (> 0) value, since tab-index=0 means "follow the system default order".
+ * The default tab order comes _after_ special tab indexes (>0).
+ * @param {number} tabIndex The index to sanitize
+ * @param {number} highestPositiveTabIndex The largest number among the tab indexes from the same context
+ * @throws An error if the tabIndex is less than 0
+ * @returns Tha sanitized tab index
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/tabindex} for further information on the tabindex and its order
+ */
+function sanitizeTabIndexInput(tabIndex, highestPositiveTabIndex) {
   if (tabIndex < 0) {
     throw new Error(
-      "Unable to sort given input. A negative value is not part of the tab order: ",
-      tabIndex
+      `Unable to sort given input. A negative value is not part of the tab order: ${tabIndex}`
     );
   }
   // 0 based tab indexes have a higher order than positive valued indicies, thus we add 1 to the max value
-  return tabIndex === 0 ? maxValue + 1 : tabIndex;
+  return tabIndex === 0 ? highestPositiveTabIndex + 1 : tabIndex;
 }
 
 function getTabIndexOfNode(targetNode) {
